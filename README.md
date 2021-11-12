c3db53854c1d3cc1fade5fb0c8e4be4118e99355
dtuankun <dtuankun@ucsd.edu>
2021-11-12 16:16:21 +0700

    add all

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Lab 8 - Starter
+
+## Question 1
+
+> In your own words: Where would you fit your automated tests in your Recipe project development pipeline?
+>
+> 1. Within a Github action that runs whenever code is pushed
+> 2. Manually run them locally before pushing code
+> 3. Run them all after all development is completed
+
+I would put automated tests within a GitHub action that runs whenever code is pushed.
+
+Essentially we obviously don't want point number 3, because we want to test along the changes that we added so we can quickly notice the bug and fix it right away.
+
+The first and second choice is whether we run it on the cloud or locally, it is better if we run them in the cloud so it doesn't make us have to wait all of the test to complete before pushing. In the cloud, we can just leave it while working with other things and come back later if there are some error.
+
+In my opinion, the best of both world is by working with Pull Request so error code will not be shipped to the main branch, and can be fixed before merging the PR.
+
+## Question 2
+
+> Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+No, we should use the unit test instead. E2E was supposed to emulate user actions from start to finish.
+
+## Question 3
+
+> Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No, the feature is too big to be tested using unit test, we should use integration testing to do that. Unit testing is supposed to test individual components, but should not test how the components interact with each other.
+
+This feature needs to be break down into different unit to be tested with unit test.
+
+## Question 4
+
+> Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes, this is something that we can test with unit testing. By testing max message, it is only involving that specific function and we are debugging on a really small scale.
+
+This test is also can be executed quickly and used as a utility that won't affect other non-related unit-tests.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,112 @@
 // unit.test.js
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
+const { isPhoneNumber, isEmail, isStrongPassword, isDate, isHexColor } =
+  functions;
 
 // TODO - Part 2
+//#region  //*=========== isPhoneNumber ===========
+describe('isPhoneNumber function should work correctly', () => {
+  test('isPhoneNumber("abcdefghijkl") with alphabet should return false', () => {
+    expect(isPhoneNumber('abcdefghijkl')).toBe(false);
+  });
+
+  test('isPhoneNumber("1231234") without dash should return false', () => {
+    expect(isPhoneNumber('1231234')).toBe(false);
+  });
+
+  test('isPhoneNumber("123-1234") with dash should return true', () => {
+    expect(isPhoneNumber('123-1234')).toBe(true);
+  });
+
+  test('isPhoneNumber("123-123-1234") with 2 dash should return true', () => {
+    expect(isPhoneNumber('123-123-1234')).toBe(true);
+  });
+});
+//#endregion  //*======== isPhoneNumber ===========
+
+//#region  //*=========== isEmail ===========
+describe('isEmail function should work correctly', () => {
+  test('isEmail("josh(at)yahoo.com") without @ should return false', () => {
+    expect(isEmail('josh(at)yahoo.com')).toBe(false);
+  });
+
+  test('isEmail("josh@yahoo") withoutdomain should return false', () => {
+    expect(isEmail('josh@yahoo')).toBe(false);
+  });
+
+  test('isEmail("josh@yahoo.com") with @ and domain should return true', () => {
+    expect(isEmail('josh@yahoo.com')).toBe(true);
+  });
+
+  test('isEmail("john_doe@gmail.com") with correct format and _ should return true', () => {
+    expect(isEmail('john_doe@gmail.com')).toBe(true);
+  });
+});
+//#endregion  //*======== isEmail ===========
+
+//#region  //*=========== isStrongPassword ===========
+// The password's first character must be a letter,
+// it must contain at least * 4 characters and no more than 15 characters and no characters other than * * letters,
+// numbers and the underscore may be used
+describe('isStrongPassword function should work correctly', () => {
+  test('isStrongPassword("1asdfasdf") with first character as number should return false', () => {
+    expect(isStrongPassword('1asdfasdf')).toBe(false);
+  });
+
+  test('isStrongPassword("abc") with less than 4 characters should return false', () => {
+    expect(isStrongPassword('abc')).toBe(false);
+  });
+
+  test('isStrongPassword("abcdefghijkl") with 12 characters should return true', () => {
+    expect(isStrongPassword('abcdefghijkl')).toBe(true);
+  });
+
+  test('isStrongPassword("abc123__43") with numbers and underscore should return true', () => {
+    expect(isStrongPassword('abc123__43')).toBe(true);
+  });
+});
+//#endregion  //*======== isStrongPassword ===========
+
+//#region  //*=========== isDate ===========
+// This regular expressions matches dates of the form XX / XX / YYYY where
+// XX can be 1 or 2 digits long and YYYY is always 4 digits long.
+describe('isDate function should work correctly', () => {
+  test('isDate("11112021") without / should return false', () => {
+    expect(isDate('11112021')).toBe(false);
+  });
+
+  test('isDate("031/20/2020") with XX 3 digits long should return false', () => {
+    expect(isDate('031/20/2020')).toBe(false);
+  });
+
+  test('isDate("1/11/2021") with XX 1 digit should return true', () => {
+    expect(isDate('1/11/2021')).toBe(true);
+  });
+
+  test('isDate("11/11/2021") with XX 2 digits should return true', () => {
+    expect(isDate('11/11/2021')).toBe(true);
+  });
+});
+//#endregion  //*======== isDate ===========
+
+//#region  //*=========== isHexColor ===========
+// Matches valid 3 or 6 character hex codes used for HTML or CSS.
+describe('isHexColor function should work correctly', () => {
+  test('isHexColor("1234") with 4 characters should return false', () => {
+    expect(isHexColor('1234')).toBe(false);
+  });
+
+  test('isHexColor("rgb(1, 2, 3)") with rgb syntax should return false', () => {
+    expect(isHexColor('rgb(1, 2, 3)')).toBe(false);
+  });
+
+  test('isHexColor("#123456") with 6 characters should return true', () => {
+    expect(isHexColor('#123456')).toBe(true);
+  });
+
+  test('isHexColor("123") with 3 characters and without # should return true', () => {
+    expect(isHexColor('123')).toBe(true);
+  });
+});
+//#endregion  //*======== isHexColor ===========
